53652c5f74d4cd8a930729c6f80b9296cf1c1c39
Dhruva <dhruvashastri12@gmail.com>
2020-03-18 22:18:15 +0530

    Added radio buttons, Display saving amount

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,65 @@ class CalcBody extends StatefulWidget {
 }
 
 class _CalcBodyState extends State<CalcBody> {
+  int selectedRadio;
   double payableAmount = 0.0;
+  String savingAmount = '0.0';
 
   var _itemEditController = TextEditingController();
-
   var _discountEditController = TextEditingController();
+
   final snackbar = SnackBar(
     content: Text('Negative value not allowed'),
   );
+
+  @override
+  void initState() {
+    super.initState();
+    selectedRadio = 0;
+    _discountEditController.addListener(_printLatestPayableAmount);
+  }
+
+  // Changes the selected value on 'onChanged' click on each radio button
+  setSelectedRadio(int val) {
+    setState(() {
+      selectedRadio = val;
+    });
+  }
+
+  _printLatestPayableAmount() {
+    print("Second text field: ${_discountEditController.text}");
+    calculateDiscount();
+  }
+
+  calculateDiscount() {
+    if (_itemEditController.text.length > 0 &&
+        _discountEditController.text.length > 0 &&
+        !_itemEditController.text.contains("-") &&
+        !_discountEditController.text.contains("-")) {
+      if (selectedRadio == 0) {
+        setState(() {
+          payableAmount = double.parse(_itemEditController.text) -
+              double.parse(_discountEditController.text);
+          savingAmount = _discountEditController.text;
+          print('Flat Discount: $payableAmount');
+        });
+      } else {
+        double cuttingPrice = double.parse(_itemEditController.text) *
+            (double.parse(_discountEditController.text) / 100);
+        setState(() {
+          payableAmount = double.parse(_itemEditController.text) - cuttingPrice;
+          savingAmount = cuttingPrice.toStringAsFixed(2);
+          print('Saving Amount: $savingAmount');
+          print('Percentage Discount: $payableAmount');
+        });
+      }
+      // _itemEditController.clear();
+      // _discountEditController.clear();
+      // FocusScope.of(context).requestFocus(FocusNode());
+    } // else {
+    //   Scaffold.of(context).showSnackBar(snackbar);
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,82 +105,95 @@ class _CalcBodyState extends State<CalcBody> {
             controller: _itemEditController,
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(Icons.cancel, color: Colors.grey[400]),
+                onPressed: () {
+                  _itemEditController.clear();
+                  payableAmount = 0.0;
+                  savingAmount = '0.0';
+                },
+              ),
               border: OutlineInputBorder(),
               labelText: 'Enter Item Price',
             ),
           ),
-          SizedBox(
-            height: 20,
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Radio(
+                value: 0,
+                groupValue: selectedRadio,
+                onChanged: (val) {
+                  print("Radio $val");
+                  setSelectedRadio(val);
+                },
+              ),
+              new Text(
+                'Flat Amount',
+                style: new TextStyle(fontSize: 16.0),
+              ),
+              new Radio(
+                value: 1,
+                groupValue: selectedRadio,
+                onChanged: (val) {
+                  print("Radio $val");
+                  setSelectedRadio(val);
+                },
+              ),
+              new Text(
+                'Percentage',
+                style: new TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
           ),
           TextField(
             controller: _discountEditController,
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(Icons.cancel, color: Colors.grey[400]),
+                onPressed: () {
+                  _discountEditController.clear();
+                  payableAmount = 0.0;
+                  savingAmount = '0.0';
+                },
+              ),              
               border: OutlineInputBorder(),
-              labelText: 'Enter Discount Amount',
+              labelText: 'Enter Discount',
             ),
           ),
           SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  if (!_itemEditController.text.contains("-") &&
-                      !_discountEditController.text.contains("-")) {
-                    setState(() {
-                      payableAmount = double.parse(_itemEditController.text) -
-                          double.parse(_discountEditController.text);
-                      
-                    });
-                    _itemEditController.clear();
-                    _discountEditController.clear();
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  } else {
-                    Scaffold.of(context).showSnackBar(snackbar);
-                  }
-                },
-                child: Text('Flat'),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  double cuttingPrice = double.parse(_itemEditController.text) *
-                      (double.parse(_discountEditController.text) / 100);
-                  
-                  if (!_itemEditController.text.contains("-") &&
-                      !_discountEditController.text.contains("-")) {
-                    setState(() {
-                      payableAmount =
-                          double.parse(_itemEditController.text) - cuttingPrice;
-                      
-                    });
-                    _itemEditController.clear();
-                    _discountEditController.clear();
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  } else {
-                    Scaffold.of(context).showSnackBar(snackbar);
-                  }
-                },
-                child: Text('Percentage'),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
+            height: 15.0,
           ),
           Text(
             'Total Payable Amount',
             style: TextStyle(fontSize: 30.0),
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             height: 10.0,
           ),
           Text(
             '$payableAmount',
-            style: TextStyle(fontSize: 45.0),
-          )
+            style: TextStyle(color: Colors.blue, fontSize: 40.0, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'You Save',
+            style: TextStyle(fontSize: 30.0),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            '$savingAmount',
+            style: TextStyle(color: Colors.blue, fontSize: 35.0, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+
         ],
       ),
     );
